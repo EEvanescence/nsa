683ab048e6b0173d80bad097c7f4877ce0dafa9b
nesa <127026836+EEvanescence@users.noreply.github.com>
2024-11-29 13:42:54 +0330

    Update sing-box.js

--- src/cores-configs/sing-box.js
+++ src/cores-configs/sing-box.js
@@ -646,7 +646,7 @@ export async function getSingBoxCustomConfig(request, env, isFragment) {
         } catch (error) {
             console.log('An error occured while parsing chain proxy: ', error);
             chainProxy = undefined;
-            await env.bpb.put("proxySettings", JSON.stringify({
+            await env.nsa.put("proxySettings", JSON.stringify({
                 ...proxySettings, 
                 outProxy: '',
                 outProxyParams: {}
